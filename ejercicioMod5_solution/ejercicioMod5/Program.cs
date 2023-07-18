46ca7a4fd96655b9f6c9a305628d49c39eba0105
saioaganuza <tasiaarmenliz@gmail.com>
2023-07-18 12:43:17 +0200

    commit subir

---
+++ ejercicioMod5_solution/ejercicioMod5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics;
+
+namespace ejercicioMod5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<Vehiculo>();
+            var v1 = new Vehiculo(1000,Vehiculo.TipoEtiqueta.CERO, 2014);
+            var v2 = new Vehiculo(2000,Vehiculo.TipoEtiqueta.sin, 2006);
+            var v3 = new Vehiculo(2000,Vehiculo.TipoEtiqueta.B, 2018);
+            var v4 = new Vehiculo(5000,Vehiculo.TipoEtiqueta.C, 2010);
+            var v5 = new Vehiculo(5500,Vehiculo.TipoEtiqueta.ECO, 2005);
+
+            lista.Add(v1);
+            lista.Add(v2);
+            lista.Add(v3);
+            lista.Add(v4);
+            lista.Add(v5);
+
+            foreach (var vehiculo in lista)
+            {
+            Console.WriteLine("El valor incrementado del vehículo es "+ v1.SacarImpuesto(vehiculo));
+            }
+            
+
+
+            
+        }
+    }
+}
---
+++ ejercicioMod5_solution/ejercicioMod5/Vehiculo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioMod5
+{
+    internal class Vehiculo
+    {
+        public enum TipoEtiqueta { 
+        sin,
+        B,
+        C,
+        ECO, 
+        CERO
+        }
+        public Vehiculo() { }
+        public Vehiculo(double precio, TipoEtiqueta etiqueta, int años)
+        {
+            Precio = precio;
+            Etiqueta = etiqueta;
+            Años = años;
+        }
+
+        public double Precio { get; set; }
+        public TipoEtiqueta Etiqueta { get; set; }
+        public int Años { get; set; }
+
+        internal double SacarImpuesto( Vehiculo v)
+        {
+            double impuesto=0;
+            
+            double valor_etiqueta=0; 
+
+            switch (v.Etiqueta)
+            {
+                case TipoEtiqueta.sin:
+                    valor_etiqueta = 0.25;
+                    break;
+                case TipoEtiqueta.B:
+                    valor_etiqueta = 0.15;
+                    break;
+                case TipoEtiqueta.C:
+                    valor_etiqueta = 0.1;
+                    break;
+                case TipoEtiqueta.ECO: 
+                    valor_etiqueta = 0.05;
+                    break;
+                case TipoEtiqueta.CERO:
+                    valor_etiqueta= 0;
+                    break;
+
+            }
+
+            impuesto = v.Precio + (DateTime.Now.Year - v.Años) * v.Precio * 0.01;
+            impuesto = impuesto + impuesto *valor_etiqueta;
+
+            // throw new NotImplementedException();
+            return impuesto;
+
+        }
+    }
+}
