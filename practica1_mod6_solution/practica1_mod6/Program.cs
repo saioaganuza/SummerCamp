98d2c15d2a9c296c9ec1d1a7f67c9f9e0ca8733a
saioaganuza <tasiaarmenliz@gmail.com>
2023-07-19 10:37:05 +0200

    practica 1 módulo 6

---
+++ practica1_mod6_solution/practica1_mod6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace practica1_mod6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<vehiculo>();
+
+            while (true) {
+
+                Console.WriteLine("Escoja el constructor con el que desea crear su coche: \n 1. Indicando el año de compra y el color. \n 2. Indicando la marca y el modelo. \n 3. Indicando el año de compra, la marca, el modelo y el color.");
+
+                int eleccion = Int32.Parse(Console.ReadLine());
+
+              
+                switch (eleccion)
+                {
+                    case 1:
+                        Console.WriteLine("Indique el año de compra: ");
+                        int año = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Indique el color del vahículo: ");
+                        string color = Console.ReadLine();
+                        var v = new vehiculo(año,color);
+                        list.Add(v);
+                        break;
+                    case 2:
+                        Console.WriteLine("Indique la marca del vahículo: ");
+                        string marca = Console.ReadLine();
+                        Console.WriteLine("Indique el modelo del vahículo: ");
+                        string modelo = Console.ReadLine();
+                        v = new vehiculo(marca,modelo);
+                        list.Add(v);
+                        break;
+                    case 3:
+                        Console.WriteLine("Indique el año de compra: ");
+                        año = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Indique el color del vahículo: ");
+                        color = Console.ReadLine();
+                        Console.WriteLine("Indique la marca del vahículo: ");
+                        marca = Console.ReadLine();
+                        Console.WriteLine("Indique el modelo del vahículo: ");
+                        modelo = Console.ReadLine();
+                        v = new vehiculo(año,marca,modelo,color);
+                        list.Add(v);
+                        break;
+                    default:
+                        Console.WriteLine("Escoja entre 1, 2 o 3. Gracias");
+                        eleccion = Int32.Parse(Console.ReadLine());
+                        break;
+                }
+                Console.WriteLine("El vehículo se ha registrado correctamente.");
+                int i= 1;
+                foreach(vehiculo v in list) {
+                    v.SacarDatos(v,i);
+                    i++;
+                        }
+                
+            }
+
+        }
+    }
+}
---
+++ practica1_mod6_solution/practica1_mod6/vehiculo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica1_mod6
+{
+    internal class vehiculo
+    {
+        public vehiculo()
+        {
+            
+        }
+
+        public vehiculo(int año, string marca, string modelo, string color)
+        {
+            Año = año;
+            Marca = marca;
+            Modelo = modelo;
+            Color = color;
+        }
+
+        public vehiculo(int año, string color)
+        {
+            Año = año;
+            Color = color;
+            Marca = "Sin especificar";
+            Modelo = "Sin especificar";
+        }
+
+        public vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Color = "Sin especificar";
+            Año = 0;
+        }
+
+        public int Año { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+
+        public void SacarDatos(vehiculo v, int pos)
+        {
+            Console.WriteLine("Vehículo " + pos + ": " + "Año:" + v.Año +  " Color:" + v.Color + " Marca:" + v.Marca + " Modelo:" + v.Modelo );
+        }
+    }
+}
